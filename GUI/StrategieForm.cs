190dcea1a8ad242cea7804fe59a26a05940568ab
Snaep <ksnaep@web.de>
2015-04-19 16:48:10 +0200

    Create StrategieForm.cs

---
+++ GUI/StrategieForm.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku {
+    public partial class StrategieForm : Form {
+        public StrategieForm() {
+            InitializeComponent();
+        }
+
+        internal string GetParameter() {
+            //-st0 -st1 -st2 ...
+            return " ";
+        }
+    }
+}
